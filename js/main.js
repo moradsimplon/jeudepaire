2e6b468f541299b88266ecb2505576b8fd8ffbde
moradsimplon <morad.bzioui@laposte.net>
2017-07-15 19:49:56 +0200

    rewrite html and css

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// variables
+var secret = document.getElementsByTagName('img');
+var resultatCarte ;
+var resultplayer;
+
+
+
+
+
+// function
+
+// for(var i=0; i< secret.length; i++){
+//   secret[i].onclick = function(){
+//     this.src= secret[0];
+//
+//   }
+// }
+
+// script
